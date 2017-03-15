e9ae747716f556a1ee3ea4a05c2801125a0795e0
mrassmann <matt.rassmann@me.com>
2017-03-14 21:01:05 -0400

    Sample Masking done

--- imageMaskingSample.py
+++ imageMaskingSample.py
@@ -1,42 +1,41 @@
 from scipy import misc
 import numpy as np
-import math
 from PIL import Image, ImageDraw
 
-image = misc.imread('problem3.jpg')
-row, col, z = image.shape
-if z == 3:
-    mask = np.zeros((row, col, 4))
-    flag = False
-    for r in range(0, row+1):
-        if r % 50 == 0:
-            flag = not flag
-        if flag:
-            mask[r:] = (1, 1, 1, 1)
-        else:
-            mask[r:] = (0, 0, 0, 0)
-    image = np.dstack((image, np.full((row, col), 255)))
+def maskImage(maskBase):
+    image = misc.imread("canvas_rose_red.gif")
+    row, col, z = image.shape
+    mask = np.array(maskBase.getdata()).reshape(maskBase.size[0], maskBase.size[1], 3)
+    if z == 3:
+        image = np.dstack((image, np.full((row, col), 255)))
+        mask = np.dstack((mask, np.full((row, col), 255)))
+    for r in range(0, row):
+        for c in range(0, col):
+            if np.all(mask[r][c] == [0, 0, 0, 255]):
+                mask[r][c] = (0, 0, 0, 0)
+            else:
+                mask[r][c] = (1, 1, 1, 1)
 
-maskedImage = np.ma.masked_array(image, mask=mask)
-maskedImage = maskedImage.filled([0, 0, 0, 0])
-print maskedImage
-misc.toimage(maskedImage, cmin=0.0, cmax=256.0).save('outfile.png')
+    print mask
+    maskedImage = np.ma.masked_array(image, mask=mask)
+    maskedImage = maskedImage.filled([0, 0, 0, 0])
+    misc.toimage(maskedImage, cmin=0.0, cmax=256.0).save('outfile.png')
 
 
-def hexagon_generator(edge_length, offset):
+def hexagon_generator():
     """Generator for coordinates in a hexagon."""
-    x, y = offset
-    for angle in range(0, 360, 60):
-        x += math.cos(math.radians(angle)) * edge_length
-        y += math.sin(math.radians(angle)) * edge_length
-        yield x, y
+    points = []
+    for k in range(6):
+        x, y = 50 + 50 * np.cos(k * np.pi / 3), 50 + 50 * np.sin(k * np.pi / 3)
+        points.extend([x, y])
+    return points
 
 def main():
     image2 = Image.new('RGB', (100, 100), 'white')
     draw = ImageDraw.Draw(image2)
-    hexagon = hexagon_generator(50, offset=(20, 0))
-    draw.polygon(list(hexagon), fill='red')
-    image2.show()
+    hexagon = hexagon_generator()
+    draw.polygon(hexagon, fill='black')
+    maskImage(image2)
 
 main()
 
